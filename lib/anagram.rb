750d7650c40adda2e0b87d6efc694eacb307e074
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-05-25 17:19:29 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(words) 
+    some_words = []
+    words.split("").collect do |x|
+      if @word.sort == x.sort 
+         some_words.push(x)
+      end
+    end
+    return some_words
+  end
+
+end
